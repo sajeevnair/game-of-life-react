ece67ec33536c026b9eb81e9c213cc71655be7ee
Sajeev Nair <sajeev.nair174@gmail.com>
2020-07-18 21:37:32 -0700

    added immer, onclick funtionality

--- src/components/Life/index.tsx
+++ src/components/Life/index.tsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react';
 import { Cell, GridContainer } from './styles';
+import produce from 'immer';
 const rowSize = 50;
 const colSize = 50;
 
 const Life = () => {
-  const [grid, setgrid] = useState(() => {
+  const [running, setRunning] = useState(false);
+  const [grid, setGrid] = useState(() => {
     const rows: number[][] = [];
     for (let i = 0; i < rowSize; i++) {
       rows.push(Array.from(Array(colSize), () => 0));
     }
     return rows;
   });
-  console.log(grid);
+  const onClickCell = (row: number, col: number) => {
+    const newGrid = produce(grid, (gridCopy: number[][]) => {
+      gridCopy[row][col] = grid[row][col] ? 0 : 1;
+    });
+    setGrid(newGrid);
+  };
+  const onClickButton = () => {
+    setRunning((running) => !running);
+  };
+
   return (
-    <GridContainer colSize={colSize}>
-      {grid.map((row, i) => row.map((col, j) => <Cell key={`${i}-${j}`} value={col} />))}
-    </GridContainer>
+    <>
+      <button onClick={onClickButton}>{running ? 'Stop' : 'Start'}</button>
+      <GridContainer colSize={colSize}>
+        {grid.map((row, i) =>
+          row.map((col, j) => <Cell key={`${i}-${j}`} value={col} onClick={() => onClickCell(i, j)} />),
+        )}
+      </GridContainer>
+    </>
   );
 };
 
